6fa36a92a2239a20400e460fbeaf72fcbe814778
FF Weinstrasse <rene@Renes-iMac.fritz.box>
2016-09-05 16:53:54 +0200

    Change Chkgw zu new GWs

--- ffws-chkgw/files/lib/gluon/ffws-chkgw/gw_test.sh
+++ ffws-chkgw/files/lib/gluon/ffws-chkgw/gw_test.sh
@@ -4,32 +4,16 @@ soll="3600"
 
 if [ $UT -gt $soll ]
 then
-	ping -c 2 -v6 fd00:ffba:ffba::199 > /dev/null 2>&1
+	ping -c 2 -v6 2a03:2260:300c:500::b > /dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
-		ping -c 2 -v6 fd00:ffba:ffba::101 > /dev/null 2>&1
+		ping -c 2 -v6 2a03:2260:300c:500::5> /dev/null 2>&1
 		if [ $? -ne 0 ]
 		then
-			ping -c 2 -v6 fd00:ffba:ffba::102 > /dev/null 2>&1
+			ping -c 2 -v6 2a03:2260:300c:500::6 > /dev/null 2>&1
 			if [ $? -ne 0 ]
 			then
-				ping -c 2 -v6 fd00:ffba:ffba::103 > /dev/null 2>&1
-				if [ $? -ne 0 ]
-				then
-					ping -c 2 -v6 fd00:ffba:ffba::104 > /dev/null 2>&1
-					if [ $? -ne 0 ]
-					then
-						ping -c 2 -v6 fd00:ffba:ffba::105 > /dev/null 2>&1
-						if [ $? -ne 0 ]
-						then
-							ping -c 2 -v6 fd00:ffba:ffba::106 > /dev/null 2>&1
-							if [ $? -ne 0 ]
-							then
-								reboot
-							fi
-						fi
-					fi
-				fi
+				reboot
 			fi
 		fi
 	fi
